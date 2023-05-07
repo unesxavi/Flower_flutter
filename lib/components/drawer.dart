2a47030130b9f7662ace2dc347c9eeaa6b5189d3
Ezzanz Youness <youness.ezzanz@sahadina.com>
2023-05-07 23:27:50 +0100

    Firebase configue

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -1,95 +1,112 @@
 import 'package:flower_flutter/constants/colors.dart';
 import 'package:flutter/material.dart';
 
+import '../pages/checkout.dart';
+import '../pages/home.dart';
+
 class FlDrawer extends StatelessWidget {
   const FlDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              children: [
-                const UserAccountsDrawerHeader(
-                  decoration: BoxDecoration(
-                    // image: DecorationImage(
-                    //   image: AssetImage("assets/images/flower.jpg"),
-                    //   fit: BoxFit.cover,
-                    // ),
-                    color: appBarGreen,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              const UserAccountsDrawerHeader(
+                decoration: BoxDecoration(
+                  // image: DecorationImage(
+                  //   image: AssetImage("assets/images/flower.jpg"),
+                  //   fit: BoxFit.cover,
+                  // ),
+                  color: appBarGreen,
+                ),
+                accountName: Text(
+                  "Aikini",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 255, 255, 255),
                   ),
-                  accountName: Text(
-                    "Aikini",
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 255, 255, 255),
+                ),
+                accountEmail: Text(
+                  "contact@aikini.com",
+                ),
+                currentAccountPicture: CircleAvatar(
+                  radius: 55,
+                  backgroundImage: AssetImage("assets/images/aikini.jpeg"),
+                ),
+              ),
+              ListTile(
+                title: const Text(
+                  "Home",
+                ),
+                leading: const Icon(
+                  Icons.home,
+                ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Home(),
                     ),
-                  ),
-                  accountEmail: Text(
-                    "contact@aikini.com",
-                  ),
-                  currentAccountPicture: CircleAvatar(
-                    radius: 55,
-                    backgroundImage: AssetImage("assets/images/aikini.jpeg"),
-                  ),
+                  );
+                },
+              ),
+              ListTile(
+                title: const Text(
+                  "My products",
                 ),
-                ListTile(
-                  title: const Text(
-                    "Home",
-                  ),
-                  leading: const Icon(
-                    Icons.home,
-                  ),
-                  onTap: () {},
+                leading: const Icon(
+                  Icons.add_shopping_cart,
                 ),
-                ListTile(
-                  title: const Text(
-                    "My products",
-                  ),
-                  leading: const Icon(
-                    Icons.add_shopping_cart,
-                  ),
-                  onTap: () {},
-                ),
-                ListTile(
-                  title: const Text(
-                    "About",
-                  ),
-                  leading: const Icon(
-                    Icons.help_center,
-                  ),
-                  onTap: () {},
-                ),
-                ListTile(
-                  title: const Text(
-                    "Logout",
-                  ),
-                  leading: const Icon(
-                    Icons.exit_to_app,
-                  ),
-                  onTap: () {},
-                ),
-              ],
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(bottom: 40),
-                  child: const Text(
-                    "Developed by Aikini © 2023",
-                    style: TextStyle(
-                      fontSize: 16,
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Checkout(),
                     ),
+                  );
+                },
+              ),
+              ListTile(
+                title: const Text(
+                  "About",
+                ),
+                leading: const Icon(
+                  Icons.help_center,
+                ),
+                onTap: () {},
+              ),
+              ListTile(
+                title: const Text(
+                  "Logout",
+                ),
+                leading: const Icon(
+                  Icons.exit_to_app,
+                ),
+                onTap: () {},
+              ),
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(bottom: 40),
+                child: const Text(
+                  "Developed by Aikini © 2023",
+                  style: TextStyle(
+                    fontSize: 16,
                   ),
-                )
-              ],
-            ),
-          ],
-        ),
-      );
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flower_flutter/provider/cart.dart';
 import 'package:flutter/material.dart';
 import 'package:flower_flutter/pages/home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:provider/provider.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
